bcd7139c09e64389acf56059b13a628c3efb9e0e
Rahil Chertara <32500120+rchertara@users.noreply.github.com>
2018-05-01 12:42:47 -0400

    ai fixed, fixed draw and resgin
    test it

--- app/src/main/java/com/example/avocado/chess_app31/PlaybackActivity.java
+++ app/src/main/java/com/example/avocado/chess_app31/PlaybackActivity.java
@@ -87,6 +87,7 @@ public class PlaybackActivity extends AppCompatActivity {
            ImageView currTile= (ImageView) grid.getChildAt(sView);
            ImageView targetTile=(ImageView) grid.getChildAt(eView);
 
+
              targetTile.setImageDrawable(currTile.getDrawable());
              currTile.setImageDrawable(null);
 
--- app/src/main/java/com/example/avocado/chess_app31/controllerView.java
+++ app/src/main/java/com/example/avocado/chess_app31/controllerView.java
@@ -8,6 +8,8 @@
 package com.example.avocado.chess_app31;
 
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class controllerView {
 
@@ -106,7 +108,16 @@ public class controllerView {
 
 
 						if(isValidMove==true) {
-							board=copyBoard;//hmmm //otherwise chnage board to this shit
+
+							List<Move> boardMoves;
+							boardMoves=board.allMoves; //keep list of moves
+							Move aiMove=copyBoard.allMoves.get(copyBoard.allMoves.size()-1);
+							board=copyBoard;
+							board.allMoves=boardMoves;
+							//board.allMoves.add(aiMove);
+
+							 //hmmm //otherwise chnage board to this shit
+
 							break;
 						}
 
